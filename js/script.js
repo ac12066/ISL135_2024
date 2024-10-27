eaa87a6c2917b7d12ddfd037e57904e860b75587
tv22002 <tv22002@ues.edu.sv>
2024-10-26 21:14:23 -0600

    Miembro C: Agregar funcionalidad JavaScript para cambiar el contenido del parrafo #descripcion cuando se haga clic en el boton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 "// script inicial ISL135_2024"
+document.getElementById("boton").addEventListener("click", function () {
+	document.getElementById("descripcion").textContent = "¡El botón ha sido presionado!";});
